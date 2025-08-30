c49a116384f74581314a83baf156af2e09fd5e50
kiojeen <kiojeen36@gmail.com>
2025-08-30 14:50:12 +0300

    Add GetErrorMessage to SystemCommerce_android
    Emergency commit that adds a new method `GetErrorMessage` to the `SystemCommerce_android` class.

--- app/src/main/java/com/tgc/sky/SystemCommerce_android.java
+++ app/src/main/java/com/tgc/sky/SystemCommerce_android.java
@@ -1,6 +1,6 @@
-// 
+//
 // Decompiled by Procyon v0.5.36
-// 
+//
 
 package com.tgc.sky;
 
@@ -10,54 +10,61 @@ import com.tgc.sky.commerce.ProductInfo;
 public class SystemCommerce_android
 {
     private static volatile SystemCommerce_android sInstance;
+    private String mError;
     SystemCommerce_android(final GameActivity activity) {
         SystemCommerce_android.sInstance = this;
     }
-    
+
     public static SystemCommerce_android getInstance() {
         return SystemCommerce_android.sInstance;
     }
-    
+
     public boolean CanMakePayments() {
         return false;
     }
-    
+
     public boolean FinishPurchase(String productIdToSystemProductId, final String s) {
         return false;
     }
-    
+
     public int GetPlatformInt() {
         return 0;
     }
-    
+
     public ProductInfo GetProductInfo(String productIdToSystemProductId) {
         return null;
     }
-    
+
     public Receipt GetReceipt() {
         return null;
     }
-    
+
     public boolean IsPurchasePending(String productIdToSystemProductId) {
         return false;
     }
-    
+
     public void LoadProducts(final String[] a) {
 
     }
-    
+
     public boolean MakePurchase(String productIdToSystemProductId) {
         return false;
     }
-    
+
     public boolean RefreshReceipt() {
         return true;
     }
-    
+
+    String GetErrorMessage() {
+        String str = this.mError;
+        this.mError = null;
+        return str;
+    }
+
     public boolean RestorePurchases() {
         return true;
     }
-    
+
     void onDestroy() {
     }
 
