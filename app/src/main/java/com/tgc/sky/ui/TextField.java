d2b7ea3a938f4fded0a01e3e663fceb25d6f62a8
MisterGatto <antoformi4+1@gmail.com>
2025-10-30 16:00:02 +0100

    chat draft fix

--- app/src/main/java/com/tgc/sky/ui/TextField.java
+++ app/src/main/java/com/tgc/sky/ui/TextField.java
@@ -1,6 +1,7 @@
 package com.tgc.sky.ui;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.GradientDrawable;
@@ -22,11 +23,10 @@ import com.tgc.sky.SystemUI_android;
 import com.tgc.sky.util.IdCounter;
 
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
-
 
 public class TextField {
     public static final int kInvalidTextFieldId = -1;
+
     private GameActivity m_activity;
     private int m_imeOptions;
     private int m_inputType;
@@ -34,18 +34,22 @@ public class TextField {
     private EditText m_textField;
     private SystemUI_android m_systemUI;
     private TextFieldLimiter m_textFieldLimiter;
-    private Rect m_hitRect = new Rect();
+    private final Rect m_hitRect = new Rect();
     private final IdCounter<Integer> m_idCounter = new IdCounter<>(-1);
+
     private String m_textBuffer = "";
     private int m_cursorPos = -1;
     private int m_selectPos = -1;
     private String m_textBufferProgram = "";
     private int m_cursorPosProgram = -1;
     private int m_selectPosProgram = -1;
+
     private boolean m_submitted = false;
     private State m_state = State.kTextFieldState_Hidden;
     private boolean m_init = false;
 
+    private static String sChatDraft = "";
+    private boolean m_isDraftEnabled = false;
 
     public enum State {
         kTextFieldState_Hidden,
@@ -56,65 +60,67 @@ public class TextField {
 
     public void initWithParams(GameActivity gameActivity) {
         this.m_activity = gameActivity;
+
         AppCompatEditText appCompatEditText = new AppCompatEditText(this.m_activity) {
 
-            @Override // android.widget.TextView, android.view.View
-            protected void onFocusChanged(boolean z, int i, Rect rect) {
-                super.onFocusChanged(z, i, rect);
-                TextField.this.m_activity.notifyEditTextFocus(z);
+            @Override
+            public boolean onKeyPreIme(int code, KeyEvent event) {
+                if (code == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
+                    InputMethodManager imm = (InputMethodManager) TextField.this.m_activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(TextField.this.m_textField.getWindowToken(), 0);
+                    TextField.this.m_textField.clearFocus();
+                    return false;
+                }
+                return super.onKeyPreIme(code, event);
             }
 
             @Override
-            protected void onSelectionChanged(int i, int i2) {
-                super.onSelectionChanged(i, i2);
-                if (!TextField.this.m_init || TextField.this.m_textField.getVisibility() == View.INVISIBLE) {
-                    return;
+            public boolean onKeyDown(int code, KeyEvent event) {
+                if (code == KeyEvent.KEYCODE_ENTER) {
+                    return true;
                 }
-                TextField.this.updateCursorPos(i, i2, Objects.requireNonNull(getText()).toString());
+                return super.onKeyDown(code, event);
+            }
+
+            private void updateCursorPos(int selStart, int selLength, String str) {
+                int caret = selStart + selLength;
+                TextField.this.m_cursorPos = TextField.this.updateCursorPosUTF8(caret, str);
+                TextField.this.m_selectPos = TextField.this.updateCursorPosUTF8(selStart, str);
             }
         };
+
         this.m_textField = appCompatEditText;
         this.m_inputType = 49153;
         this.m_imeOptions = 33554436;
 
-        /*
-        this.m_inputType = InputType.TYPE_CLASS_TEXT |
-                   InputType.TYPE_TEXT_FLAG_MULTI_LINE |
-                   InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD;
-
-        this.m_imeOptions = EditorInfo.IME_ACTION_DONE |
-                    EditorInfo.IME_FLAG_NO_EXTRACT_UI;
-        */
-
-        appCompatEditText.addTextChangedListener(new TextWatcher() { // from class: com.tgc.sky.ui.TextField.2
-            @Override // android.text.TextWatcher
-            public void afterTextChanged(Editable editable) {
-            }
-
-            @Override // android.text.TextWatcher
-            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-            }
-
-            @Override // android.text.TextWatcher
-            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-                if (!TextField.this.m_init || TextField.this.m_textField.getVisibility() == View.INVISIBLE) {
-                    return;
-                }
+        appCompatEditText.addTextChangedListener(new TextWatcher() {
+            @Override public void afterTextChanged(Editable s) {}
+            @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!TextField.this.m_init || TextField.this.m_textField.getVisibility() == View.INVISIBLE) return;
                 TextField.this.updateBuffer(TextField.this.m_textField.getText().toString());
+                if (TextField.this.m_isDraftEnabled) {
+                    TextField.sChatDraft = TextField.this.m_textField.getText().toString();
+                }
             }
         });
+
         this.m_textField.setTextColor(ViewCompat.MEASURED_STATE_MASK);
         this.m_textField.setTextSize(18.0f);
         this.m_textField.setFitsSystemWindows(true);
+
         this.m_textFieldLimiter = new TextFieldLimiter();
         resizeTextField(true, 0);
-        GradientDrawable gradientDrawable = new GradientDrawable();
-        this.m_textField.setBackground(gradientDrawable);
-        gradientDrawable.setCornerRadius(Utils.dp2px(8.0f));
-        gradientDrawable.setColor(Color.argb(0.5f, 1.0f, 1.0f, 1.0f));
-        gradientDrawable.setStroke(1, Color.argb(0.8f, 1.0f, 1.0f, 1.0f));
+
+        GradientDrawable bg = new GradientDrawable();
+        this.m_textField.setBackground(bg);
+        bg.setCornerRadius(Utils.dp2px(8.0f));
+        bg.setColor(Color.argb(128, 255, 255, 255));
+        bg.setStroke(1, Color.argb(204, 255, 255, 255));
+
         this.m_textField.setImeOptions(33554432);
-        this.m_textField.setHintTextColor(-12303292);
+        this.m_textField.setHintTextColor(0xFF444444);
         this.m_textField.setGravity(8388627);
         this.m_textField.setInputType(16385);
         this.m_textField.setVisibility(View.INVISIBLE);
@@ -130,26 +136,34 @@ public class TextField {
                 );
                 git.artdeell.skymodloader.MainActivity.onKeyboardCompleteNative(textView.getText().toString());
                 textView.setText("");
+                if (TextField.this.m_isDraftEnabled) {
+                    TextField.sChatDraft = "";
+                }
                 TextField.this.m_submitted = true;
                 return true;
             }
             return false;
         });
+
         this.m_textField.setFilters(new InputFilter[]{this.m_textFieldLimiter});
         this.m_activity.getBridgeView().addView(this.m_textField);
-        this.m_activity.addOnKeyboardListener((z, i) -> {
-            if (z) {
-                TextField.this.resizeTextField(false, i);
+
+        this.m_activity.addOnKeyboardListener((visible, kbHeight) -> {
+            if (visible) {
+                TextField.this.resizeTextField(false, kbHeight);
                 return;
             }
             if (!TextField.this.m_submitted) {
+                if (TextField.this.m_isDraftEnabled) {
+                    TextField.sChatDraft = TextField.this.m_textField.getText().toString();
+                }
                 TextField.this.m_activity.onKeyboardCompleteNative("", TextField.this.m_isCallbackTextfield, true);
             }
             TextField.this.hideTextField();
         });
+
         this.m_init = true;
     }
-
 
     private boolean isVirtualKeyboard() {
         return this.m_activity.getResources().getConfiguration().keyboard == 1;
@@ -161,29 +175,30 @@ public class TextField {
         }
     }
 
-    public int showTextFieldWithPromptAsync(final String str, final String str2, final int i, final int i2, final boolean z) {
-        int iTryActivate;
-        if (getState() != State.kTextFieldState_Hidden || (iTryActivate = tryActivate()) == -1) {
-            return -1;
+    public int showTextFieldWithPromptAsync(final String prompt, final String initial, final int maxChars, final int maxBytes, final boolean isCallback) {
+        int id;
+        if (getState() != State.kTextFieldState_Hidden || (id = tryActivate()) == -1) {
+            return kInvalidTextFieldId;
         }
         setState(State.kTextFieldState_RequestShow);
-        this.m_activity.runOnUiThread(() -> {
-            showTextFieldWithPrompt(str, str2, i, i2, z);
-            setState(State.kTextFieldState_Showing);
-        });
-        return iTryActivate;
+        this.m_activity.runOnUiThread(() -> showTextFieldWithPrompt(prompt, initial, maxChars, maxBytes, isCallback));
+        return id;
     }
 
-    public void showTextFieldWithPrompt(String str, String str2, int i, int i2, boolean z) {
-        initBufferAndCursorPos(str2);
-        this.m_isCallbackTextfield = z;
+    public void showTextFieldWithPrompt(String prompt, String initial, int maxChars, int maxBytes, boolean isCallback) {
+        initBufferAndCursorPos(initial);
+        this.m_isCallbackTextfield = isCallback;
         this.m_submitted = false;
-        this.m_textFieldLimiter.maxByteSize = i2;
-        this.m_textFieldLimiter.maxCharacters = i;
-        this.m_textField.setText(str2);
-        EditText editText = this.m_textField;
-        editText.setSelection(0, editText.getText().length());
-        this.m_textField.setHint(str);
+        this.m_textFieldLimiter.maxByteSize = maxBytes;
+        this.m_textFieldLimiter.maxCharacters = maxChars;
+
+        this.m_isDraftEnabled = ((this.m_imeOptions & EditorInfo.IME_MASK_ACTION) == EditorInfo.IME_ACTION_SEND);
+        String __initial = (m_isDraftEnabled && (initial == null || initial.isEmpty())) ? sChatDraft : initial;
+
+        this.m_textField.setText(__initial);
+        EditText et = this.m_textField;
+        et.setSelection(0, et.getText().length());
+        this.m_textField.setHint(prompt);
         this.m_textField.setAlpha(0.0f);
         this.m_textField.setInputType(this.m_inputType);
         this.m_textField.setImeOptions(this.m_imeOptions);
@@ -192,6 +207,7 @@ public class TextField {
         this.m_textField.setFocusable(true);
         this.m_textField.setFocusableInTouchMode(true);
         this.m_textField.requestFocus();
+
         resizeTextField(false, 0);
         showVirtualKeyboard();
         setState(State.kTextFieldState_Showing);
@@ -210,7 +226,8 @@ public class TextField {
             this.m_activity.onKeyboardCompleteNative("", this.m_isCallbackTextfield, true);
         }
         if (getState() != State.kTextFieldState_Hidden) {
-            ((InputMethodManager) this.m_activity.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(this.m_textField.getWindowToken(), 0);
+            ((InputMethodManager) this.m_activity.getSystemService(Context.INPUT_METHOD_SERVICE))
+                    .hideSoftInputFromWindow(this.m_textField.getWindowToken(), 0);
             GameActivity.hideNavigationFullScreen(this.m_activity.getBridgeView());
             this.m_textField.setInputType(0);
             this.m_textField.setImeOptions(0);
@@ -226,23 +243,46 @@ public class TextField {
         clearId();
     }
 
-    public void resizeTextField(boolean z, int i) {
-        int dp2px = Utils.dp2px(8.0f);
-        Rect GetSafeAreaInsets = this.m_activity.GetSafeAreaInsets();
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(-1, -2);
-        if (z) {
-            layoutParams.addRule(10, -1);
-            layoutParams.topMargin = i + dp2px;
+    public void resizeTextField(boolean top, int keyboardHeight) {
+        int pad = Utils.dp2px(8.0f);
+        Rect insets = this.m_activity.GetSafeAreaInsets();
+
+        RelativeLayout.LayoutParams lp =
+                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        if (top) {
+            lp.addRule(RelativeLayout.ALIGN_PARENT_TOP, -1);
+            lp.topMargin = insets.top + pad;
+            lp.bottomMargin = pad;
         } else {
-            layoutParams.addRule(12, -1);
-            layoutParams.bottomMargin = i + dp2px;
+            lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, -1);
+            lp.bottomMargin = keyboardHeight + pad;
         }
-        layoutParams.leftMargin = GetSafeAreaInsets.left + dp2px;
-        layoutParams.rightMargin = GetSafeAreaInsets.right + dp2px;
-        this.m_textField.setLayoutParams(layoutParams);
-        this.m_textField.setPadding(dp2px, dp2px, dp2px, dp2px);
+        lp.leftMargin = insets.left + pad;
+        lp.rightMargin = insets.right + pad;
+
+        this.m_textField.setLayoutParams(lp);
+        this.m_textField.setPadding(pad, pad, pad, pad);
         this.m_textField.setFitsSystemWindows(true);
         this.m_textField.setAlpha(1.0f);
+    }
+
+    public String getTextBuffer() {
+        return this.m_textBuffer;
+    }
+
+    public String getPrompt() {
+        CharSequence hint = this.m_textField.getHint();
+        return hint == null ? "" : hint.toString();
+    }
+
+    public int getCursorPos() {
+        return this.m_cursorPos;
+    }
+
+    public int getSelectPos() {
+        return this.m_selectPos;
     }
 
     public float getTextFieldHeight() {
@@ -258,54 +298,42 @@ public class TextField {
         return str.substring(0, i).getBytes(StandardCharsets.UTF_8).length;
     }
 
-    public void updateCursorPos(int i, int i2, String str) {
-        this.m_cursorPos = updateCursorPosUTF8(i, str);
-        this.m_selectPos = i != i2 ? updateCursorPosUTF8(i2, str) : -1;
-    }
-
-    public void updateBuffer(String str) {
+    private void updateBuffer(String str) {
         this.m_textBuffer = str;
     }
 
-    private void initBufferAndCursorPos(String str) {
+    private void initBufferAndCursorPos(String initial) {
+        this.m_textBuffer = initial == null ? "" : initial;
+        this.m_cursorPos = -1;
+        this.m_selectPos = -1;
+        this.m_cursorPosProgram = -1;
+        this.m_selectPosProgram = -1;
+    }
+
+    public void setTextBufferAndCursorPosProgram(String str) {
         updateBuffer(str);
         this.m_textBufferProgram = this.m_textBuffer;
         int length = str.length();
-        updateCursorPos(length, length, str);
+        this.m_cursorPos = updateCursorPosUTF8(length, str);
+        this.m_selectPos = updateCursorPosUTF8(length, str);
         this.m_cursorPosProgram = this.m_cursorPos;
         this.m_selectPosProgram = this.m_selectPos;
     }
 
-    public String getTextBuffer() {
-        return this.m_textBuffer;
-    }
-
-    public String getPrompt() {
-        return this.m_textField.getHint().toString();
-    }
-
-    public int getCursorPos() {
-        return this.m_cursorPos;
-    }
-
-    public int getSelectPos() {
-        return this.m_selectPos;
-    }
-
     public int tryActivate() {
-        int iIntValue;
+        int id;
         synchronized (this) {
-            iIntValue = ((Integer) this.m_idCounter.TryActivate()).intValue();
+            id = this.m_idCounter.TryActivate().intValue();
         }
-        return iIntValue;
+        return id;
     }
 
     public boolean isIdActive(int i) {
-        boolean zIsActiveId;
+        boolean active;
         synchronized (this) {
-            zIsActiveId = this.m_idCounter.IsActiveId(Integer.valueOf(i));
+            active = this.m_idCounter.IsActiveId(Integer.valueOf(i));
         }
-        return zIsActiveId;
+        return active;
     }
 
     public void clearId() {
@@ -320,5 +348,23 @@ public class TextField {
 
     public void setState(State state) {
         this.m_state = state;
+    }
+
+    private static class TextFieldLimiter implements InputFilter {
+        public int maxCharacters = Integer.MAX_VALUE;
+        public int maxByteSize = Integer.MAX_VALUE;
+
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end,
+                                   android.text.Spanned dest, int dstart, int dend) {
+            return null;
+        }
+    }
+
+    private static class Utils {
+        static int dp2px(float dp) {
+            float density = Resources.getSystem().getDisplayMetrics().density;
+            return (int) (dp * density + 0.5f);
+        }
     }
 }
