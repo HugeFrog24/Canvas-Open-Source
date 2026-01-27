17f7ef59dffe2b08a08c7400c95ebc091882b653
MisterGatto <antoformi4+1@gmail.com>
2026-01-27 18:04:05 +0100

    Update update checker URL for Canvas

--- app/src/main/java/git/artdeell/skymodloader/updater/CanvasUpdaterService.java
+++ app/src/main/java/git/artdeell/skymodloader/updater/CanvasUpdaterService.java
@@ -13,7 +13,7 @@ public class CanvasUpdaterService extends AbstractUpdaterService {
 
     @Override
     protected String getUpdateCheckerURL() {
-        return "https://api.github.com/repos/skyprotocol/canvas-distribution/releases/latest";
+        return "https://api.github.com/repos/MisterGatto/Canvas-Open-Source-test/releases/latest";
     }
 
     @Override
