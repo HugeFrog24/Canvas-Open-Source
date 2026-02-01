a18fcda6092739e2e4e760ac05e5702dba6c1a73
MisterGatto <antoformi4+1@gmail.com>
2026-02-01 21:42:35 +0100

    1.7.0

--- app/src/main/java/com/tgc/sky/BuildConfig.java
+++ app/src/main/java/com/tgc/sky/BuildConfig.java
@@ -6,7 +6,7 @@ public class BuildConfig {
     public static String SKY_STAGE_NAME = "Live";
     public static String SKY_SERVER_HOSTNAME = "live.radiance.thatgamecompany.com";
     public static String SKY_VERSION = "0.17.5";
-    public static String SKY_BUILD_ACCESS_KEY = "1743442606-0c9545db24db0fee924a028c500c75f968145e6f15d2535ff2ca414798eba96a";
+    public static String SKY_BUILD_ACCESS_KEY = "1743442606-3de96bd2ce8644c9d7144775e38bb8de13b3ac9b15e2984b0304445c5bbe0174";
 
     public static int VERSION_CODE = 192395;
 }
